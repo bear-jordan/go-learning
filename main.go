8a57658413b1d0cabe313a89445297bd4fb06a74
bear-jordan <bearjordan@gmail.com>
2025-01-22 10:56:56 -0500

    CLN: refactor to cleanup

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
-package main
+/*
+Author: Bear Jordan
+Description: Utility to color standard input
+Usage: Usage: `color -color=<color> [-verbose=true] <message>
+*/
 
+package main
 
 import (
     "flag"
@@ -7,55 +12,67 @@ import (
     "os"
 )
 
-func MakeMessage(color string, message string) string {
+func MakeMessage(color, message string) string {
     return fmt.Sprintf("%s%s\033[0m", color, message)
-
 }
 
-func ParseArgs() (string, string, bool) {
-    // Init flags
+func ParseArgs() (string, string, bool, bool) {
+    // Init args
     selectedColor := flag.String("color", "white", "Color to display message")
     verbose := flag.Bool("verbose", false, "Set verbosity")
+    help := flag.Bool("help", false, "Display help menu")
     flag.Parse()
-    
-    // Check args are correct
-    if flag.NArg() != 1 {
-        fmt.Fprintf(os.Stderr, "Usage: `color -color=white [-verbose=true] <message>`\r\n")
-        os.Exit(1)
+
+    // Validate args
+    if *help || flag.NArg() != 1 {
+        return *selectedColor, "", *verbose, true
     }
 
-    // Get message
-    message := flag.Arg(0)
+    return *selectedColor, flag.Arg(0), *verbose, *help
+}
 
-    return *selectedColor, message, *verbose
+func ShowHelp(validColors map[string]string) {
+    // Print out help menu
+    fmt.Fprintln(os.Stderr, "Usage: `color -color=<color> [-verbose=true] \"<message>\"`")
+    fmt.Fprintln(os.Stderr, "\nValid colors:")
+    for color := range validColors {
+        fmt.Fprintf(os.Stderr, "  - %s\n", color)
+    }
 }
 
 func main() {
-    // Parse args
-    selectedColor, message, verbose := ParseArgs()
-    if verbose {
-        fmt.Fprint(os.Stderr, fmt.Sprintf("Picking a color: `%s`\r\n", selectedColor))
-    }
-
-    // Define colors
-    var validColors = map[string]string {
-        "black": "\033[0;30m",
-        "red": "\033[0;31m",
-        "green": "\033[0;32m",
+    // Init colors
+    validColors := map[string]string{
+        "black":  "\033[0;30m",
+        "red":    "\033[0;31m",
+        "green":  "\033[0;32m",
         "yellow": "\033[0;33m",
-        "blue": "\033[0;34m",
+        "blue":   "\033[0;34m",
         "purple": "\033[0;35m",
-        "cyan": "\033[0;36m",
-        "white": "\033[0;37m",
+        "cyan":   "\033[0;36m",
+        "white":  "\033[0;37m",
     }
 
-    // Pick a color
+    // Parse args
+    selectedColor, message, verbose, help := ParseArgs()
+
+    // Show help if needed
+    if help {
+        ShowHelp(validColors)
+        os.Exit(0)
+    }
+
+    // Validate selected color
+    if verbose {
+        fmt.Fprintf(os.Stderr, "Picking a color: `%s`\n", selectedColor)
+    }
+
     color, ok := validColors[selectedColor]
     if !ok {
-        fmt.Fprint(os.Stderr, "Color not found.\r\n")
+        fmt.Fprintln(os.Stderr, "Color not found.")
         color = validColors["white"]
     }
-    fmt.Println(MakeMessage(color, message))
 
-    os.Exit(0)
+    // Print message
+    fmt.Println(MakeMessage(color, message))
 }
