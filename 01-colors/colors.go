d0d283dcebc3057bba262f7b2737b3cd8a60d68c
bear-jordan <bearjordan@gmail.com>
2025-01-21 19:17:22 -0500

    ENH: working colors

--- 01-colors/colors.go
+++ 01-colors/colors.go
@@ -4,5 +4,28 @@ import "fmt"
 
 
 func PrintColor() {
-    fmt.Println("Print colors")
+    // Define a color -> ansii map
+    type color struct {
+        name string
+        color string
+    }
+
+    // Define colors
+    var colors = []color {
+        {name: "black", color: "\033[0;30m"},
+        {name: "red", color: "\033[0;31m"},
+        {name: "green", color: "\033[0;32m"},
+        {name: "yellow", color: "\033[0;33m"},
+        {name: "blue", color: "\033[0;34m"},
+        {name: "purple", color: "\033[0;35m"},
+        {name: "cyan", color: "\033[0;36m"},
+        {name: "white", color: "\033[0;37m"},
+    }
+
+    // Give a message (default to hello)
+    for _, color := range colors {
+        var message = fmt.Sprintf("%s%s\033[0m", color.color, color.name)
+        fmt.Println(message)
+    }
+    // Pick a color
 }
